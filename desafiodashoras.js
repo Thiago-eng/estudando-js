a32cfff68f70fd36ad0b564eafc874f77480ec40
thiago-chamberlain <thiago.span@hotmail.com>
2025-12-15 20:12:22 -0300

    novo commit

---
+++ desafiodashoras.js
@@ -0,0 +1,11 @@
+let hora = 22
+
+if (hora < 12) {
+    console.log("Bom dia!")
+} else if (hora < 18) {
+    console.log("Boa tarde!")
+} else if (hora < 23) {
+    console.log("Boa noite!")
+} else {
+    console.log("Hora inválida!")
+}
