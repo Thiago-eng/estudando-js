f5ccabc9ecd6beee7ac314c10a1d772132e8a2fe
thiago-chamberlain <thiago.span@hotmail.com>
2025-12-25 18:39:17 -0300

    Estudando...

---
+++ desafiofunctionfix.js
@@ -0,0 +1,16 @@
+function nivelCargaBateria(percentual) {
+    if (percentual < 0 || percentual > 100) {
+        return "Percentual inválido"
+    } else if (percentual <= 15) {
+        return "Carga crítica"
+    } else if (percentual <= 40) {
+        return "Carga baixa"
+    } else if (percentual <= 70) {
+        return "Carga média"
+    } else {
+        return "Carga alta"
+    }
+}
+
+console.log("Seu dispositivo está com: ")
+console.log(nivelCargaBateria(63)) // output: Carga média
