9b56b75191e33683f1f9a978adac7bf276facdcc
thiago-chamberlain <thiago.span@hotmail.com>
2026-01-22 20:25:36 -0300

    registrando o aprendizado

---
+++ começandoifelse2part1.js
@@ -0,0 +1,8 @@
+let idade = 20
+let temCNH = true
+
+if (idade >= 18 && temCNH) {
+        console.log("Pode dirigir")
+    } else {
+        console.log("Não pode dirigir, pois não tem CNH")
+    }
