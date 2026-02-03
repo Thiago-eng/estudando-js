e9b928336e9ae08ab6c21fa85d0bc8bb8df7e5c9
thiago-chamberlain <thiago.span@hotmail.com>
2026-02-02 23:04:31 -0300

    código errado

---
+++ desafioX.js
@@ -0,0 +1,11 @@
+let logado = false
+let moderador = true
+let banido = false
+
+if (logado === true && banido === false || moderador === true && banido === false) {
+    console.log("Pode logar no sistema")
+} else {
+    console.log("Não pode logar no sistema")
+}
+
+// Eu quis apenas aproveitar a chance para colocar essa opção alternativa anterior.
