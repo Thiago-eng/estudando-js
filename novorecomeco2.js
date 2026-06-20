fdd32e17ab1d2afa55d43f1e615d6ae95c79518e
thiago-chamberlain <thiago.span@hotmail.com>
2026-06-19 23:40:43 -0300

    novorecomeco2 consistencia mantida

---
+++ novorecomeco2.js
@@ -0,0 +1,10 @@
+let nota = 8;
+let aluno = "Thiago";
+
+if (nota < 5) {
+    console.log(aluno + " está reprovado.");
+} else if (nota < 7) {
+    console.log(aluno + " está em recuperação.");
+} else {
+    console.log(aluno + " está aprovado.");
+}
