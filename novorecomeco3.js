f1f4f3d1925251cc550f09b40e91dd47c6a7b777
thiago-chamberlain <thiago.span@hotmail.com>
2026-06-20 22:54:58 -0300

    novorecomeco3

---
+++ novorecomeco3.js
@@ -0,0 +1,9 @@
+let idade = 16;
+let possuiCNH = true;
+let nome = "Thiago";
+
+if (idade >= 18 && possuiCNH === true) {
+    console.log(nome + " pode dirigir.");
+} else {
+    console.log(nome + " não pode dirigir.");
+}
