28d37cd7f6bed74cf6a55b94a35e7560e7a7087d
thiago-chamberlain <thiago.span@hotmail.com>
2026-06-20 23:15:28 -0300

    novorecomeco4

---
+++ novorecomeco4.js
@@ -0,0 +1,9 @@
+let diaDeFolga = true;
+let feriado = false;
+let nome = "Thiago";
+
+if (diaDeFolga || feriado) {
+    console.log(nome + " pode descansar.");
+} else {
+    console.log(nome + " precisa trabalhar.");
+}
